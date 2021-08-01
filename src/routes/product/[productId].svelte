17656caecd44bb84ec844e7b31cad3fa43a4c55a
irg1008 <ivansudevlop@gmail.com>
2021-08-01 17:15:06 +0200

    Added vercel deploy configuration

---
+++ src/routes/product/[productId].svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+
+	
+</script>
+
+<svelte:head>
+	<title>{$page.params.productId}</title>
+</svelte:head>
+
+<p>Displaying information about product: {$page.params.productId}</p>
