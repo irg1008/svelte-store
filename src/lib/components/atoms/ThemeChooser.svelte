5d2e9b68a83a6153d1fc9580fba482cc68964e46
irg1008 <ivansudevlop@gmail.com>
2021-08-01 21:23:38 +0200

    Theme done, getting now to custom store and apis

--- src/lib/components/atoms/ThemeChooser.svelte
+++ src/lib/components/atoms/ThemeChooser.svelte
@@ -2,18 +2,18 @@
 	import { theme, themes } from "$lib/stores/theme";
 </script>
 
-<h2>Choose the theme (current: {$theme})</h2>
-
-{#each themes as iTheme}
-	<label for="{iTheme}"></label>
-	<input
-		id="{iTheme}"
-		type="radio"
-		group="{themes}"
-		name="themes"
-		value="{iTheme}"
-		checked="{iTheme === $theme}"
-		on:change="{() => theme.set(iTheme)}"
-	/>
-	{iTheme}
-{/each}
+<div class="flex flex-col gap-2 p-2">
+	<h2>Choose the theme (current: {$theme})</h2>
+	{#each themes as iTheme}
+		<label>
+			<input
+				type="radio"
+				name="themes"
+				value={iTheme}
+				checked={iTheme === $theme}
+				on:change={() => theme.set(iTheme)}
+			/>
+			{iTheme}</label
+		>
+	{/each}
+</div>
